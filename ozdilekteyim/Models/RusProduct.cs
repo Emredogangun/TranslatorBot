05b16f800a16e70cf37b590a470b17f33a520954
EMRE DOĞANGÜN <50294340+Emredogangun@users.noreply.github.com>
2021-02-02 21:01:29 +0300

    Created

---
+++ ozdilekteyim/Models/RusProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ozdilekteyim.Models
+{
+    public class RusProduct
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool State { get; set; }
+        public int ProductID { get; set; }
+        public Product Product { get; set; }
+        public string LanguageCode { get; set; } = "ru-ru";
+        public string ShopPHPID { get; set; }
+        public string ShopPHPName { get; set; }
+        public bool ShopPHPState { get; set; }
+
+    }
+}
---
+++ ozdilekteyim/Program.cs
@@ -0,0 +1,21 @@
+﻿using ozdilekteyim.Context;
+using ozdilekteyim.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ozdilekteyim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            DriverChrome driverChrome = new DriverChrome();
+            driverChrome.translateUrl("https://translate.google.com/?sl=tr&tl=ru&op=translate");
+
+        }
+
+    }
+}
